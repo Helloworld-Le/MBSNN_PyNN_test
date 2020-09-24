49bdf38bf2b247dcd09fdcf62e821c864ea23114
Helloworld-Le <43879506+Helloworld-Le@users.noreply.github.com>
2020-09-24 20:47:59 +0100

    Update PYNN_PN2EN.py

--- PYNN_PN2EN.py
+++ PYNN_PN2EN.py
@@ -1,15 +1,10 @@
-import time
-import csv
-import numpy as np
+
 import matplotlib as plt
 from copy import *
 import os
 import yaml
 import scipy.io as sio
-from brian2 import *
-import brian2tools
-from brian2tools import *
-import pandas as pd
+
 from scipy.sparse import rand
 import brian2
 import random
@@ -21,25 +16,46 @@ import argparse
 
 import pickle
 
-import brian2genn
 import argparse
 
 from numpy import arange
 from pyNN.utility import get_simulator, init_logging, normalized_filename
 import numpy as np
+import numpy as np
+import pylab as plt
+import spynnaker8 as sim
+from pyNN.utility.plotting import Figure, Panel
 
+# SpiNNaker setup
+sim.setup(timestep=1.0, min_delay=1.0, max_delay=14)
+# +---------------------------------------------------------------------------+
+# | General Parameters                                                        |
+# +---------------------------------------------------------------------------+
+
+# Population parameters
+model = sim.IF_curr_exp
+sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 50)
+cell_params = {'cm': 0.25,
+               'i_offset': 0.0,
+               'tau_m': 20.0,
+               'tau_refrac': 2.0,
+               'tau_syn_E': 5.0,
+               'tau_syn_I': 5.0,
+               'v_reset': -70.0,
+               'v_rest': -65.0,
+               'v_thresh': -50.0
+               }
 # === Configure the simulator ================================================
 
 # sim, options = get_simulator(("--plot-figure", "Plot the simulation results to a file.", {"action": "store_true"}),
 #                              ("--debug", "Print debugging information"))
-import pyNN.nest as sim
+
 options = argparse.Namespace(debug=None, plot_figure=True, simulator='nest')
 
 
 if options.debug:
     init_logging(None, debug=True)
 
-sim.setup(timestep=0.01, min_delay=1.0)
 
 # prefs.devices.genn.cuda_path = '/usr/local/cuda-10.0'
 # prefs.devices.genn.path = '/home/le/Installation/packages/genn-3.2.0'
@@ -86,11 +102,11 @@ class MB_LE(object):
         self.kc_neuron_idx = range(0 , nb_kc , nb_kc / 10)
 
         self.spike_source = sim.Population(nb_pn, sim.SpikeSourceArray(spike_times = dvs_input))
-        self.pns = sim.Population(nb_pn , sim.IF_cond_exp(tau_m = 50.0) , label = "PN")
-        self.kcs = sim.Population(nb_kc , sim.IF_cond_exp(tau_m = 50.0) , label = "KC")
-        self.kcs_a = sim.Population(nb_kc , sim.IF_cond_exp(tau_m = 50.0) , label = "KC_A")
-        self.ens = sim.Population(nb_en , sim.IF_cond_exp(tau_m = 50.0) , label = "EN")
-        self.ens_a = sim.Population(nb_en , sim.IF_cond_exp(tau_m = 50.0) , label = "EN_A")
+        self.pns = sim.Population(nb_pn , model(**cell_params), label = "PN")
+        self.kcs = sim.Population(nb_kc , model(**cell_params) , label = "KC")
+        self.kcs_a = sim.Population(nb_kc ,model(**cell_params), label = "KC_A")
+        self.ens = sim.Population(nb_en ,model(**cell_params) , label = "EN")
+        self.ens_a = sim.Population(nb_en , model(**cell_params), label = "EN_A")
 
         self.dvs2pn = sim.Projection(self.spike_source , self.pns , sim.OneToOneConnector() ,
                                     sim.StaticSynapse(weight = 0.1 , delay = 1.0) ,
